07092c17d83ffeb09cd5f49d8a7f3a079985cfc6
Septian Dika <83001459+sed1ka@users.noreply.github.com>
2026-05-02 10:16:29 +0700

    chore: add app string constants

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,5 @@
+abstract class AppStrings {
+  static const String generalError = 'Something went wrong';
+  static const String dataError = 'The data error occurred';
+  static const String invalidDataError = 'Invalid data';
+}

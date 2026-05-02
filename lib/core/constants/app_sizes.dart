355baf3c0dd502cfa0095f652403c5246aa075d9
Septian Dika <83001459+sed1ka@users.noreply.github.com>
2026-05-02 10:16:18 +0700

    chore: add app size constants

---
+++ lib/core/constants/app_sizes.dart
@@ -0,0 +1,41 @@
+/// TEXT SIZE PROPERTIES
+abstract class TextSize {
+  static const double headerLarge = 29;
+  static const double headerMedium = 26;
+  static const double headerSmall = 23;
+
+  static const double titleLarge = 20;
+  static const double titleMedium = 18;
+  static const double titleSmall = 16;
+
+  static const double bodyLarge = 14.5;
+  static const double bodyMedium = 13.5;
+  static const double bodySmall = 12;
+
+  static const double subtitleLarge = 11;
+  static const double subtitleMedium = 10;
+  static const double subtitleSmall = 9;
+}
+
+/// ICON SIZE PROPERTIES
+abstract class IconSize {
+  static const double large = 24;
+  static const double medium = 18;
+  static const double small = 15;
+}
+
+/// LAYOUT SIZE PROPERTIES
+abstract class LayoutSize {
+  static const double kAppBarHeight = 58;
+  static const double kAppBarSmallHeight = 52;
+  static const double kTabBarHeight = 45;
+  static const double kExpandedHeight = 100;
+  static const double bottomNavBarHeight = 52;
+  static const double bottomNavBarIconSize = 22;
+}
+
+/// RESPONSIVE BREAKPOINTS
+abstract class Breakpoints {
+  static const double mobile = 480;
+  static const double tablet = 1024;
+}

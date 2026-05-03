38efe89624a2a2a6328a7669ad8e739cc74e967e
Septian Dika <83001459+sed1ka@users.noreply.github.com>
2026-05-03 18:05:47 +0700

    chore: add app image constants, assets, and lottie dependency

---
+++ lib/core/constants/app_images.dart
@@ -0,0 +1,8 @@
+final class AppImages {
+  const AppImages._();
+
+  static const String _basePath = 'assets/images';
+
+  static const String arrowBumpingAnimation =
+      '$_basePath/arrow_bumping_animation.zip';
+}
